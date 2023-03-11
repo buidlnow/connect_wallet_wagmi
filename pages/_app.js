c4eda83ac0bf5d572252a7e55c3e3cb6f45938ac
razacodespython <srh.zaidi@outlook.com>
2023-03-11 18:45:27 +0100

    wagmi setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
 import '@/styles/globals.css'
+import { WagmiConfig, createClient } from 'wagmi';
+import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
+import { ConnectKitProvider, getDefaultClient } from 'connectkit';
 
+const client = createClient(
+  getDefaultClient({
+    appName: 'ConnectKit Next.js demo',
+    //infuraId: process.env.NEXT_PUBLIC_INFURA_ID,
+    //alchemyId:  process.env.NEXT_PUBLIC_ALCHEMY_ID,
+    chains: [mainnet, polygon, optimism, arbitrum],
+  })
+);
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={client}>
+      <ConnectKitProvider>
+        <Component {...pageProps} />
+      </ConnectKitProvider>
+    </WagmiConfig>
+  );
 }
